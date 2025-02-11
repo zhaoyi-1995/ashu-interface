af9ec7f63986d73053b1460da24af8ea2ca5151c
zhaoyi <492613075@qq.com>
2025-02-11 14:10:28 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: {
+    main: resolve('src/index.tsx')
+  },
+  output: {
+    path: resolve(process.cwd(), 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'swc-loader'
+        }
+      }
+    ]
+  }
+}
