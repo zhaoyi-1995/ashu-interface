73464ff0dc8c9527f3fa581a76e342aa34829c2b
zhaoyi <492613075@qq.com>
2025-02-12 17:52:32 +0800

    feat: webpack 基础配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const { resolve } = require('path');
+const merge = require('webpack-merge')
+
+const argv = require('yargs-parser')(process.argv.slice(2))
+const _mode = argv.mode || 'development'
+const _mergeConfig = require(`./config/webpack.${_mode}.js`)
+
 const webpackBaseConfig = {
   entry: {
     main: resolve('src/index.tsx')
