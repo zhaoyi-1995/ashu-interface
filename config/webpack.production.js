4d730ea980407f9e4118249905467f56ac5e56f9
zhaoyi-1995 <492613075@qq.com>
2025-02-22 00:37:22 +0800

    aa

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -12,7 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'ashu', // 设置 HTML 文件的标题
       filename: 'index.html', // 输出的 HTML 文件名称
-      template: resolve(__dirname, '../src/prod.html') // 使用的模板文件
+      template: resolve(__dirname, '../src/index_prod.html') // 使用的模板文件
     }),
   ]
 }
