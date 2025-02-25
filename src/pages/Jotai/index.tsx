dc2206c22e95d0badfe5d7d96883c248c46bd3c0
zhaoyi <492613075@qq.com>
2025-02-25 16:41:50 +0800

    jotai-immer得使用

--- src/pages/Jotai/index.tsx
+++ src/pages/Jotai/index.tsx
@@ -1,15 +1,17 @@
 // src/components/JotaiDemo.tsx
 import { atom, useAtom } from 'jotai';
+import { atomWithImmer } from 'jotai-immer';
 
 // 定义用户类型
 interface User {
   name: string;
   age: number;
+  likes?: string[]
 }
 
 // 创建 Atom
 const countAtom = atom(0); // 计数器 Atom，初始值为 0
-const userAtom = atom<User>({ name: 'Alice', age: 25 }); // 用户信息 Atom
+const userAtom = atomWithImmer<User>({ name: 'Alice', age: 25 }); // 用户信息 Atom
 
 const JotaiDemo = () => {
   console.log('jotai更新了')
@@ -24,8 +26,21 @@ const JotaiDemo = () => {
 
   // 更新用户信息
   const updateUser = () => {
-    setUser({ name: 'Bob', age: 30 });
+    setUser((draf) => {
+      draf.name = '阿树'
+    });
   };
+  const updateUserByObj = () => {
+    setUser((draf) => {
+      draf.name = '阿树'
+      draf.likes = ['打篮球']
+    });
+  };
+
+  const updateUserByData = () => {
+    setUser({name: '阿树', age: 18});
+  };
+
 
   return (
     <div className="p-4">
@@ -60,11 +75,24 @@ const JotaiDemo = () => {
         <h2 className="text-lg">用户信息</h2>
         <p>姓名: {user.name}</p>
         <p>年龄: {user.age}</p>
+        <p>爱好: {user.likes?.join('---')}</p>
         <button
           onClick={updateUser}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          className="mr-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
         >
           更新用户信息
+        </button>
+        <button
+          onClick={updateUserByData}
+          className="mr-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          数据直接修改
+        </button>
+        <button
+          onClick={updateUserByObj}
+          className="mr-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          属性是复杂类型
         </button>
       </div>
     </div>
