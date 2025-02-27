4ca5281b0bdc05f6402fc2aa5417d7f3d60059c1
zhaoyi-1995 <492613075@qq.com>
2025-02-28 00:47:37 +0800

    请求数据

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,26 @@
+import { useImmer } from "@/hooks/useImmer";
+import { useEffect } from "react";
+type BooksListItem = {
+  id: string | number,
+  name: string,
+  desc?: string,
+  auth: string,
+  price: number
+}
+
 const Home = () => {
-  return <>Home</>;
+  const [bookList, setBookList] = useImmer<BooksListItem[]>([])
+  useEffect(() => {
+    fetch('/api/bookList')
+    .then((res) => res.json())
+    .then((result) => setBookList(result.data))
+    .catch((err) => console.error("Error:", err));
+  }, [])
+
+  return <>
+    <h2>书籍列表</h2>
+    {bookList.map(item => <div>{item.name} ---- {item.auth}</div> )}
+  </>;
 };
 
 export default Home;
